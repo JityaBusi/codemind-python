80c14860059ff9b4d74ed3ff7c5d06c9e3071c8c
JityaBusi <119723522+JityaBusi@users.noreply.github.com>
2023-05-24 18:53:13 +0530

    rows = int(input()) if rows < 3:     print("The pattern is not possible") else:     for i in range(rows):         for j in range(0, i + 1):             print('*', end = '')         print()     for i in range(1, rows):         for j in range(i, rows):             print('*', end = '')         print()

---
+++ Half_diamond_star_pattern.py
@@ -0,0 +1,12 @@
+rows = int(input())
+if rows < 3:
+    print("The pattern is not possible")
+else:
+    for i in range(rows):
+        for j in range(0, i + 1):
+            print('*', end = '')
+        print()
+    for i in range(1, rows):
+        for j in range(i, rows):
+            print('*', end = '')
+        print()
